2611d5894d7bd2348ca9d7f34732c1ea54ece1dc
Skullomanic84 <jhshiloh@gmail.com>
2023-04-25 12:48:44 +0200

    git url updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <img src={logo} alt="sumz_logo" className="w-28 object-contain"/>
 
     <button type="button" onClick={() => window.open(
-      `https://github.com/skullomanic`
+      `https://github.com/Skullomanic84`
     )} className="black_btn">
     Github
     </button>
